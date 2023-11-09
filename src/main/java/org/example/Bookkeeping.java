f55e1b7d97452c0b2d3ad9805c7063fb6c04764e
kriskensy <kriskensy@proton.me>
2023-11-09 18:27:59 +0100

    Dodanie interfejsu IObjectWithContract

---
+++ src/main/java/org/example/Bookkeeping.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Bookkeeping {
+    public void printTransferOrder(IObjectWithContract pwc) {
+        System.out.println("Transfer order");
+        System.out.println("Name and surname: ");
+        pwc.print();
+        System.out.println("Transfer amount: " + pwc.paycheck());
+        System.out.println();
+    }
+}
