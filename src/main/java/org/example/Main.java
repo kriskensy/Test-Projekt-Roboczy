6e67faf91132de6effec7a8ea6de6799b73b7c31
kriskensy <kriskensy@proton.me>
2023-11-10 13:55:16 +0100

    Klasy z projektami, Agreement - duza powtarzalnosc kodu!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,23 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        FamilyDoctor fd = new FamilyDoctor("Adam", "Rodzinny", 100);
-        fd.printTransferOrder();
+        Agreement agreement = new Agreement();
 
-        SpecialistDoctor sd = new SpecialistDoctor("Adam", "Specjalista", 100);
-        sd.printTransferOrder();
+        AuditProject auditProject = new AuditProject("Project 1", "Description 1",
+                100, 1, 1);
+
+        agreement.printAgreement(auditProject);
+
+        ProgrammingProject programmingProject = new ProgrammingProject("Project 2", "Description 2",
+                200, 2, 2,
+                2, 2, 2);
+
+        agreement.printAgreement(programmingProject);
+
+        HelpdeskProgrammingProject helpdeskProgrammingProject = new HelpdeskProgrammingProject("Project 3",
+                "Description 3", 300, 3, 3,
+                3, 3, 3, 3);
+
+        agreement.printAgreement(helpdeskProgrammingProject);
     }
 }
